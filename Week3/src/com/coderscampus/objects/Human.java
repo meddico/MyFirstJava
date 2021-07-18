c08384a70608b1223806c1393ecae4e19d2b971f
medkes1@gmail.com <medkes1@gmail.com>
2021-07-17 20:26:25 -0400

    week3 leason contiuation

--- Week3/src/com/coderscampus/objects/Human.java
+++ Week3/src/com/coderscampus/objects/Human.java
@@ -2,9 +2,29 @@ package com.coderscampus.objects;
 
 public class Human {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
+		String gender;
+		String height;
+		String weight;
+		String eyecolor;
+		
+		
+	String getInfo ( ) {
+		return "Height: " + height +", Weight: " + weight + 
+				", EyeColor: " + eyecolor +", Gender: " + gender;
+		
 	}
+		
+	void setProperties (String _gender, String _height,
+			String _weight, String _eyecolor) {
+		
+		gender = _gender;
+		height = _height;
+		weight = _weight;
+		eyecolor = _eyecolor;
+				
+	}
+	
 
 }
--- Week3/src/com/coderscampus/objects/RunnableClass.java
+++ Week3/src/com/coderscampus/objects/RunnableClass.java
@@ -4,7 +4,41 @@ public class RunnableClass {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Test");
+		// System.out.println("Test");//
+		//Data type, variable name, assignment operation//
+		
+		Human Medhanepage = new Human();
+		
+		
+		Medhanepage.eyecolor = "black";
+		Medhanepage.gender = "male";
+		Medhanepage.height = "5'7\"";
+		Medhanepage.weight = "200";
+		
+		System.out.println(Medhanepage.getInfo());
+		
+		
+		Human Johnpage = new Human();
+		
+		Johnpage.eyecolor = "blue";
+		Johnpage.gender = "male";
+		Johnpage.height = "5'10\"";
+		Johnpage.weight = "178";
+		
+		System.out.println(Johnpage.getInfo());
+		
+		Human selupage = new Human();
+		
+		selupage.eyecolor = "blue";
+		selupage.gender = " female";
+		selupage.height = "5.5\"";
+		selupage.weight = "144";
+		
+		System.out.println(selupage.getInfo());
+		
+		
+		
+		
 		
 	}
 
